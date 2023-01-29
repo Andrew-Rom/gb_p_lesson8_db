6a5458a8cc6b9b7af8a1bd4c9ed18ff108960c97
And-Rom <you@example.com>
2023-01-29 17:54:04 +0300

    Fixed nofile case

--- rw_mod.py
+++ rw_mod.py
@@ -6,19 +6,50 @@ import csv
 db_file = 'database.csv'
 db_book = []
 last_id = 0
+blank_list = ['ID', 'Фамилия', 'Имя', 'Отчество', 'Телефон', 'Email', 'Дата рождения', 'Курс', 'Группа',
+              'Преподаватель']
 
 
 def read_file():
-    global db_book, db_file, last_id
-    if os.path.exists(db_file):
-        with open(db_file, mode='r', encoding='utf-8', newline='') as file:
-            reader = csv.reader(file)
-            db_book = [line for line in reader]
-        last_id = int(db_book[-1][0])
-        return db_book
-    else:
-        log.logging.error('Incorrect file')
-        ui.show_error_file()
+    global db_book, db_file, last_id, blank_list
+    while True:
+        if os.path.exists(db_file):
+            with open(db_file, mode='r', encoding='utf-8', newline='') as file:
+                reader = csv.reader(file)
+                db_book = [line for line in reader]
+            last_id = int(db_book[-1][0])
+            return db_book
+        else:
+            log.logging.error('Incorrect file')
+            ui.show_error_file()
+            ask_user = input('Хотите создать новую базу данных (y/n): ')
+            if ask_user.lower() == 'y':
+                with open(db_file, mode='w', encoding='utf-8', newline='') as file:
+                    writer = csv.writer(file)
+                    writer.writerow(blank_list)
+                log.logging.info('New datafile was created')
+                id_num = last_id + 1
+                first_name = input('Введите фамилию: ')
+                last_name = input('Введите имя: ')
+                middle_name = input('Введите отчество: ')
+                tel = input('Введите номер телефона: ')
+                email = input('Введите Email: ')
+                day_of_birth = input('Введите день рождения: ')
+                course = input('Введите курс: ')
+                group = input('Введите группу: ')
+                teacher = input('Введите данные преподавателя: ')
+                new_person = [str(id_num), first_name, last_name, middle_name, tel, email, day_of_birth, course, group,
+                              teacher]
+                add_data(new_person)
+                print('Данные успешно добавлены!')
+                input('Для продолжения нажмите кнопку ввода...')
+                break
+            elif ask_user.lower() == 'n':
+                ui.show_exit_message()
+                exit()
+            else:
+                log.logging.error('Incorrect selection')
+                ui.show_error_menu()
 
 
 def write_file(new_db):
